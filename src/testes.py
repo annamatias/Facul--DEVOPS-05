cd828dd1d5cc828cd8b38724421fb471c7ae7742
annamatias <anna.matias@aluno.faculdadeimpacta.com.br>
2020-06-28 14:43:03 -0300

    atualizando jogo da velha

--- src/testes.py
+++ src/testes.py
@@ -1,22 +1,23 @@
+#preciso rever o teste, está com a versão antiga do teste. Ou seja, preciso refazer!!!
 import jogovelha
 import sys
 
-erroInicializar = False
+erroMenu = False
 
 
-jogo = jogovelha.inicializar()
+jogo = jogovelha.menu()
 
 if len(jogo) != 3:
-	erroInicializar = True
+	erroMenu = True
 else:
 	for linha in jogo:
 		if len(linha) != 3:
-			erroInicializar = True
+			erroMenu = True
 		else:
 			for elemento in linha:
 				if elemento != '.':
-					erroInicializar = True
-if erroInicializar:
+					erroMenu = True
+if erroMenu:
 	sys.exit(1)
 else:
 	sys.exit(0)
